5e2fee167321ce8f101d708bbdfac3b5768c28bf
Ryan Helgerson <ryan.helgerson@gmail.com>
2018-04-03 17:33:00 -0500

    HTML finished, logic next.

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+//========================VARIABLES===============================
+
+var playerScore
+
+var targetScore
+
+var wins
+
+var losses
+
+var crystals = {
+    blue: {
+        name: "Blue",
+        value: 0,
+    },
+    green: {
+        name: "Green",
+        value: 0,
+    },
+    red: {
+        name: "Red",
+        value: 0,
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0,
+    }
+
+}
+
+
+//========================LOGIC====================================
+
+//Create a random number
+
+//Start game 
+    //assign a random number between 18-100 to the number box
+
+    //assign four hidden random numbers between 1-12 to the four crystals
+
+//If the user score exceeds that of the random number, the user loses and the loss counter increments by 1, call game start function
+
+//Or else, if the user score equates to that of the random number, the user wins and the win counter increments by 1, call game start function
+
+//When the user clicks on a crytal, the corresponding value of the crystal is appended to the user's score.
+
+    //Call check winner function
+
